bae95246b36f6f003cb304118ea4aa36746e6bd8
Nishchal Gautam <gautam.nishchal@gmail.com>
2019-07-17 00:01:11 +0700

    style(nl): add new line after imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {Probot} from "probot";
 import {App} from "./App";
 import {getAppConfig} from "./AppConfig";
+
 const appConfig = getAppConfig();
 const probot = new Probot(appConfig);
 probot.load(App.handle);
