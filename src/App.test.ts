2ebf78fb9af809ade97476fb248b090549d4ffb3
Nishchal Gautam <nishchal.gautam@agoda.com>
2020-10-22 14:21:50 +0700

    test(app): add test for fail condition

--- src/App.test.ts
+++ src/App.test.ts
@@ -108,4 +108,35 @@ describe("App", () => {
         await app.handleEvent();
         expect(commentMock).toHaveBeenCalled();
     });
+    it("should not update comment if original issue was result of an error", async () => {
+        const payloadHelperMock: IPayloadHelper = {
+            getNewBody: jest.fn(),
+            getOriginalBody: () => "## There was error processing your body\n\nThe exact error message is the following"
+        };
+        const commentMock = jest.fn();
+        const ghHelper: IGithubIssueHelper = {
+            comment: commentMock,
+            updateBody: () => undefined as any
+        };
+        const log = jest.fn();
+        const mockContext = {
+            payload: {
+                issue: {}
+            },
+            issue: (payload: { body: string }): IPayload => ({
+                number: 1,
+                repo: "",
+                owner: "",
+                body: payload.body
+            }),
+            log: {
+                debug: log,
+                info: () => undefined as any
+            }
+        };
+        const app = new App(ghHelper, payloadHelperMock, mockContext as any);
+        await app.handleEvent();
+        expect(commentMock).not.toHaveBeenCalled();
+        expect(payloadHelperMock.getNewBody).not.toHaveBeenCalled();
+    });
 });
